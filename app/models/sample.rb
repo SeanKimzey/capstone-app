eee8dbaf43da9df5fbd1ae868ad25f4ba1b3a84c
Sean Kimzey <sean.kimzey@gmail.com>
2018-04-25 22:04:38 -0500

    file upload working and saving to the database but not displaying image on index action

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -8,5 +8,20 @@ class Sample < ApplicationRecord
     content_type: {
       content_type: ["image/jpeg", "image/gif", "image/png"]
     }
+
+
+    def json
+
+      {
+        id: id,
+        name: name, 
+        sample_type: sample_type,
+        bpm: bpm, 
+        key: key, 
+        sample_rate: sample_rate, 
+        bit_depth: bit_depth,
+        image: image
+      }
+    end
 end
  

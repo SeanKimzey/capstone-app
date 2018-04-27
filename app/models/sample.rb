f15d12c20ad5d342c45ca9e7d231b8b1ca772e0a
Sean Kimzey <sean.kimzey@gmail.com>
2018-04-26 23:33:02 -0500

    upload working with index image rendering. def as_json method was causing issues

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -3,25 +3,28 @@ class Sample < ApplicationRecord
   has_many :category_samples
   has_many :categories, through: :category_samples
   has_many :orders
-  has_attached_file :image 
+  has_attached_file :image
   validates_attachment :image,
     content_type: {
       content_type: ["image/jpeg", "image/gif", "image/png"]
     }
 
 
-    def json
+  def as_json
 
-      {
-        id: id,
-        name: name, 
-        sample_type: sample_type,
-        bpm: bpm, 
-        key: key, 
-        sample_rate: sample_rate, 
-        bit_depth: bit_depth,
-        image: image
-      }
-    end
+    {
+      id: id,
+      name: name, 
+      sample_type: sample_type,
+      bpm: bpm, 
+      key: key, 
+      sample_rate: sample_rate, 
+      bit_depth: bit_depth,
+      image: image.url,
+      the: "hello"
+  
+    }
+  end
+
 end
  

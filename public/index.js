c7a9b88e4fe77841f285a051df0cbf8dc8e37739
Sean Kimzey <sean.kimzey@gmail.com>
2018-04-29 17:06:18 -0500

    fix name field not saving to db and not clearing

--- public/index.js
+++ public/index.js
@@ -1,4 +1,79 @@
 /* global Vue, VueRouter, axios */
+var LogoutPage = {
+  template: "<h1>Logout</h1>",
+  created: function() {
+    axios.defaults.headers.common["Authorization"] = undefined;
+    localStorage.removeItem("jwt");
+    router.push("/");
+  }
+};
+
+var LoginPage = {
+  template: "#login-page",
+  data: function() {
+    return {
+      email: "",
+      password: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        auth: { email: this.email, password: this.password }
+      };
+      axios
+        .post("/user_token", params)
+        .then(function(response) {
+          axios.defaults.headers.common["Authorization"] =
+            "Bearer " + response.data.jwt;
+          localStorage.setItem("jwt", response.data.jwt);
+          router.push("/");
+        })
+        .catch(
+          function(error) {
+            this.errors = ["Invalid email or password."];
+            this.email = "";
+            this.password = "";
+          }.bind(this)
+        );
+    }
+  }
+};
+
+var SignupPage = {
+  template: "#signup-page",
+  data: function() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+        password_confirmation: this.passwordConfirmation
+      };
+      axios
+        .post("/v1/users", params)
+        .then(function(response) {
+          router.push("/login");
+        })
+        .catch(
+          function(error) {
+            this.errors = error.response.data.errors;
+          }.bind(this)
+        );
+    }
+  }
+};
+
 
 var HomePage = {
   template: "#home-page",
@@ -20,7 +95,7 @@ var HomePage = {
       input = document.getElementById("fileUploadInput");
       if (input.files.length > 0) {
         var formData = new FormData();
-        formData.append("name", this.name);
+        formData.append("name", this.newSample.name);
         formData.append("sample_type", this.newSample.sample_type);
         formData.append("bpm", this.newSample.bpm);
         formData.append("key", this.newSample.key);
@@ -33,7 +108,7 @@ var HomePage = {
           .then(function(response) {
             console.log(response);
             console.log(this);
-            this.name = "";
+            this.newSample.name = "";
             this.newSample.sample_type = "";
             this.newSample.bpm = "";
             this.newSample.key = "";
@@ -50,7 +125,12 @@ var HomePage = {
 };
 
 var router = new VueRouter({
-  routes: [{ path: "/", component: HomePage }],
+  routes: [{ path: "/", component: HomePage },
+    { path: "/signup", component: SignupPage },
+    { path: "/login", component: LoginPage },
+    { path: "/logout", component: LogoutPage }
+  ],
+      
   scrollBehavior: function(to, from, savedPosition) {
     return { x: 0, y: 0 };
   }
